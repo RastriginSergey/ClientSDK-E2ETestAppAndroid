7b73674125dca3ab8eebcdab13a702cecb7e21fa
Ruslan <ruslan.ibragimov@vonage.com>
2020-03-03 16:30:11 +0200

    first e2e app

---
+++ AppBasic/app/src/androidTest/java/com/vonage/e2etestappbasic/StaticTextUITest.kt
@@ -0,0 +1,40 @@
+package com.vonage.e2etestappbasic
+
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class StaticTextUITest {
+
+    private lateinit var stringToCheck: String
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+
+    @Before
+    fun initValidString() {
+        // Specify a valid string.
+        stringToCheck = "Welcome to Nexmo Client E2E Test app"
+    }
+
+    @Test
+    fun changeText_sameActivity() {
+        onView(withId(R.id.main_activity_text_view))
+            .check(matches(withText(stringToCheck)))
+
+    }
+}
